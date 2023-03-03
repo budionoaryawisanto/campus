9a74d79efa10e5ee2b75236ec7fb007305a8d8ff
budionoaryawisanto <budionoarya.w@gmail.com>
2023-03-03 15:35:07 +0700

    menambahkan hero dan content

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = (props) => {
+  return (
+    <div className='content'>
+          <img src={props.img} alt="content"/>
+          <div className="detail-content">
+              
+          </div>
+    </div>
+  )
+}
+
+export default Content
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,36 @@
 import {useState} from 'react'
 import search from '../images/search.png'
 import Navbar from './Navbar'
+import hero from '../images/placeholder.jpg'
+import Content from './Content'
+import content1 from '../images/placeholder1.jpg'
 
 const Home = () => {
+  var curentDate = new Date()
   return (
     <div className='body'>
       <Navbar/>
         <form action="/search" className='form-search'>
               <label htmlFor="search">Eksplorasi websites, orang dan lokasi</label>
               <div className="search-box">
+                  <input type="search" name="search" placeholder='What are you looking for ?' id="search" />
                   <img src={search} alt="search" width='30px' height="30px" className='search-button'/>
-                  <input type="search" name="search" placeholder='Search here...' id="search" />
               </div>
       </form>
+      <div className="container-hero">
+        <img src={hero} alt="hero" />
+        <div className="detail-hero">
+          <p>Anna Osherov, an assistant director at MIT.nano, helps researchers use state-of-the-art tools to probe materials’ nanoscale properties. She was attracted to materials science because of its breadth and interdisciplinary perspective — as she puts it, “Everything around us is material.”</p>
+          <div className="action-hero">
+            <h4>{curentDate.toLocaleDateString()}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="title-content">MORE FROM NCIT COMMUNITY</div>
+      <div className="container-content">
+        <Content img={content1} />
+        <Content img={content1} />
+      </div>
     </div>
   )
 }
