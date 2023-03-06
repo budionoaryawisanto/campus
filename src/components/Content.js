3a7ddc2e0367d76bed100ad77c873351e8d0202d
budionoaryawisanto <budionoarya.w@gmail.com>
2023-03-06 19:06:48 +0700

    fix error

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Content = (props) => {
+  const Navigate = useNavigate()
   return (
     <button  onClick={() => Navigate(props.tujuan)} className='content'>
           <img src={props.img} alt="content"/>
